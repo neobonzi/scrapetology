599e7adad3a9bd2278de000562307f3cb60b7af7
James Bilous <JBillz@pcp142983pcs.wireless.calpoly.edu>
2016-02-09 13:35:23 -0800

    Added some code to visit pages

--- scrape.py
+++ scrape.py
@@ -1,10 +1,14 @@
 from lxml import html
 import requests
 
+wikipediaBaseURI = 'https://en.wikipedia.org')
+
+data = {}
 page = requests.get('https://en.wikipedia.org/wiki/List_of_American_Civil_War_battles')
 tree = html.fromstring(page.content)
-tree.make_links_absolute('https://en.wikipedia.org')
+tree.make_links_absolute(wikipediaBaseURI)
 
+## Extract general battle data
 battles = tree.xpath('//table[2]/tr/td[1]/a[1]/text()')
 battleLinks = tree.xpath('//table[2]/tr/td[1]/a[1]/@href')
 startDates = tree.xpath('//table[2]/tr/td[2]/span[2]/text()')
@@ -13,5 +17,15 @@ cwsac = tree.xpath('//table[2]/tr/td[4]/center/text()')
 
 tuples = zip(battles, startDates, states, cwsac, battleLinks)
 for item in tuples:
-    print(item[0] + " - " + item[1] + " - " + item[2] + " (" + item[3] + ")" + "\n\t" + item[4]) 
+    data[item[0]] = { 'startDate' : item[1], 'state' : item[2], 'cwsac' : item[3], 'url' : item[4]}
 
+for key in data:
+    value = data[key]
+    print(key + " - " + value['startDate'] + " - " + value['state'] + " (" + value['cwsac'] + ")" + "\n\t" + value['url']) 
+
+## Visit battle pages and extract mroe data
+
+#for key in data:
+#    battlePage = requests.get(data[key][url])
+#    tree = html.fromstring(page.content)
+#    tree.make_links_absolute(wikipediaBaseURI)
