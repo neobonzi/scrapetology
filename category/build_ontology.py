4f8023404e5c9b39fab1d5e60623afa588d2b07c
jmcgover <jmcgover@calpoly.edu>
2016-03-12 11:40:13 -0800

    starting the category parser

---
+++ category/build_ontology.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import pickle
+import os
+import sys
+import logging
+
+import json
+
+sys.path.append(os.getcwd())
+
+def main():
+    return 0
+
+if __name__ == '__main__':
+    rtn = main()
+    sys.exit(rtn)
