713ab426329398a3a1eabcdaa3cdcf1589143428
jmcgover <jmcgover@calpoly.edu>
2016-03-15 05:49:58 -0700

    I think I have my parser abstraction working

--- category/build_ontology.py
+++ category/build_ontology.py
@@ -4,10 +4,17 @@ import pickle
 import os
 import sys
 import logging
+LOGGER = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+LOGGER.addHandler(handler)
+LOGGER.setLevel(logging.DEBUG)
 
 from bs4 import BeautifulSoup as bs
 import urllib
 from urllib import request
+import re
 
 import json
 from json import JSONEncoder
@@ -22,9 +29,10 @@ DELIM='~'
 INDENT=4
 
 class Instance(object):
-    def __init__(self, href, title):
+    def __init__(self, base_url, href):
+        self.base_url = base_url
         self.href = href
-        self.title = title
+        self.title = Instance.get_title(href)
         self.parents = set()
         return
     def __eq__(self, other):
@@ -43,9 +51,10 @@ class Instance(object):
             return False
 
 class Category(object):
-    def __init__(self, href, title):
+    def __init__(self, base_url, href):
+        self.base_url = base_url
         self.href = href
-        self.title = title
+        self.title = Category.get_title(href)
         self.parents = set()
         self.children = set()
         self.instances = set()
@@ -57,7 +66,7 @@ class Category(object):
     def __str__(self):
         return self.__dict__.__str__()
     def get_title(href):
-        return href.replace('/wiki/','').replace('Category:','')
+        return href.replace('/wiki/','')
     def add_parent(self, parent):
         if parent not in self.parents:
             self.parents.add(parent)
@@ -77,14 +86,87 @@ class Category(object):
         else:
             return False
 
+class WikiParser(object):
+    def __init__(self, base_url):
+        self.base_url = base_url
+    def __str__(self):
+        return 'WikiParser'
+    def get_page(self, url):
+        assert(False)
+    def get_category_pages(self, category_url):
+        assert(False)
+
+class HTMLWikiParser(WikiParser):
+    def __init__(self, base_url):
+        self.base_url = base_url
+    def __str__(self):
+        return 'HTMLWikiParser'
+    def get_page(self, url):
+        soup = None
+        LOGGER.debug('FETCHING:{}'.format(url))
+        try:
+            read_page = request.urlopen(url)
+        except urllib.error.HTTPError:
+            LOGGER.error('urllib.error.HTTPError:{}'.format(full_url))
+            raise
+        soup = bs(read_page, "lxml")
+        return soup
+    def get_categories(self):
+        assert(False)
+    def get_instances(self):
+        assert(False)
+
+class StarWarsHTMLWikiParser(HTMLWikiParser):
+    def __str__(self):
+        return 'StarWarsHTMLWikiParser'
+    def get_category_pages(self, category_href):
+        # GET FIRST PAGE
+        LOGGER.debug('CATEGORY:{}'.format(category_href))
+        soups = []
+        full_url = self.base_url + category_href
+        soup = self.get_page(full_url)
+        soups.append(soup)
+        # 
+        re_next_href = '{}.*?{}'.format(category_href,'#mw-subcategories')
+        next_tag = soup.findAll('a', href=re.compile(re_next_href))
+        while next_tag:
+            next_href = None
+            # (I'm sorry...)
+            for n in next_tag:
+                for e in n:
+                    if 'next' in e:
+                        next_href = n['href']
+                        break
+                if next_href:
+                    break
+            full_url = self.base_url + next_href
+            soup = self.get_page(full_url)
+            soups.append(soup)
+        return soups
+    def get_categories(self, soups):
+        categories = []
+        for soup in soups:
+            [categories.append(c) for c in soup.findAll('a', class_=CAT_ELEMENT_TAG)]
+        return categories
+    def get_instances(self, soups):
+        instances = []
+        for soup in soups:
+            mw_pages = soup.findAll("div", id="mw-pages")
+            if mw_pages:
+                [instances.append(i) for i in mw_pages[0].findAll("a") if i.get('title') and i.get('href')]
+        return instances;
+
 class Ontology(object):
-    CHILD_LIMIT = 0
+    CHILD_LIMIT = 5
     num_children = 0
-    def __init__(self, base_url, base_category_url):
+    def __init__(self, base_url, base_category_url, parser_class):
         self.base = base_url
-        self.base_category = Category.get_title(base_category_url)
         self.base_category_url = base_category_url
-        self.name = base_url.replace('http://','').split('.')[0] + '_' + self.base_category.replace('wiki/','')
+        self.parser = parser_class(base_url)
+
+        self.base_category = Category.get_title(base_category_url)
+        self.name = base_url.replace('http://','').split('.')[0] \
+            + '_' + self.base_category.replace('wiki/','')
         self.categories = None
         self.instances = None
         return
@@ -143,12 +225,14 @@ class Ontology(object):
             return
 
         # GRAB AND READ PAGE
-        soup = self.get_category_page(category_url)
+        #soup = self.get_category_page(category_url)
+        soups = self.parser.get_category_pages(category_url)
 
         # TRAVERSE CHILDREN
-        children = self.get_child_categories(soup)
-        if children:
-            for c in children:
+        #children = self.get_child_categories(soup)
+        child_categories = self.parser.get_categories(soups)
+        if child_categories:
+            for c in child_categories:
                 c_href = c['href']
                 c_title = Category.get_title(c_href)
                 assert(c_href)
@@ -158,11 +242,12 @@ class Ontology(object):
                 if not seen:
                     self.build_ontology_rec(c_href, title, path)
         # GET INSTANCES
-        instances = self.get_child_instances(soup);
+        #instances = self.get_child_instances(soup);
+        instances = self.parser.get_instances(soups);
         if instances:
             for i in instances:
                 i_href = i.get('href')
-                i_title = i.get('title')
+                i_title = Instance.get_title(i_href)
                 assert(i_href)
                 assert(i_title)
                 instance = None
@@ -192,6 +277,8 @@ class OntologyJSONEncoder(JSONEncoder):
     def default(self, obj):
         if isinstance(obj, set):
             return [x for x in obj]
+        if isinstance(obj, WikiParser):
+            return obj.__str__()
         elif isinstance(obj, Instance):
             return obj.__dict__
         elif isinstance(obj, Category):
@@ -200,8 +287,11 @@ class OntologyJSONEncoder(JSONEncoder):
             return obj.__dict__
         return JSONEncoder.default(self, obj)
 
+
+
 def main():
-    onto = Ontology(START_BASE_URL, START_CAT_URL)
+    parser = StarWarsHTMLWikiParser('fuck')
+    onto = Ontology(START_BASE_URL, START_CAT_URL, StarWarsHTMLWikiParser)
     json_filename = onto.get_json_name()
     pickle_filename = onto.get_pickle_name()
     cats = onto.get_ontology()
