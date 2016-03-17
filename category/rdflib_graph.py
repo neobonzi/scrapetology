976f09f1bce0f9b6b3df5a90cc0ad972e93f281e
jon doughty <jdoughty@calpoly.edu>
2016-03-17 10:37:25 -0700

    cleanup

--- category/rdflib_graph.py
+++ category/rdflib_graph.py
@@ -32,12 +32,10 @@ def strip_invalid(text):
     ir4 = ir3.replace("^", "")
     return ir4
 
-# NOTE: for now instances are subClassOf their parents, this is probably not the best practice and should be changed so that instances are a rdf:type of the class they belong to. However, can't seem to get the code working.
 def add_instance(graph, inst_dict, key, class_of):
     title = strip_invalid(inst_dict[key].title)
     href = inst_dict[key].href
     graph_add(graph, URIRef(SWDB[title]), RDF.type, OWL.NamedIndividual)
-    # NOTE: line below should be fixed so instances are not subClasses
     graph_add(graph, URIRef(SWDB[title]), RDFS.subClassOf, SWDB[class_of])
     graph_add(graph, URIRef(SWDB[title]), OWL.title, Literal(title))
     graph_add(graph, URIRef(SWDB[title]), OWL.href, Literal(href))
@@ -70,6 +68,18 @@ def sparql_query(graph):
     for row in qres:
         print(row)
 
+def print_slice(graph, sw_categories, key, sw_instances):
+    # DEBUG - take a small slice of the dicitonary for testing
+    n_items = islice(sw_categories.keys(), 0, 10)
+    for key in n_items:
+        add_class(graph, sw_categories, key, sw_instances)
+
+def print_graph(graph_filename):
+    # DEBUG - print the data for testing
+    with open(graph_filename) as file:
+        for line in file:
+            print(line, end='')
+        
 def build_graph(args):
     graph = Graph()
     sw_ontology = import_pickle_file()
@@ -79,11 +89,6 @@ def build_graph(args):
     # add the ontology
     graph_add(graph, URIRef('http://example.org/star_wars_ontology.owl'), RDF.type, OWL.Ontology)
 
-    # DEBUG - take a small slice of the dicitonary for testing
-    # n_items = islice(sw_categories.keys(), 0, 10)
-    # for key in n_items:
-    #     add_class(graph, sw_categories, key, sw_instances)
-
     # iterate through dictionary and build graph
     for key in sw_categories.keys():
         add_class(graph, sw_categories, key, sw_instances)
@@ -92,19 +97,15 @@ def build_graph(args):
     graph.bind("owl", OWL)
     graph.bind("swdb", SWDB)
 
-    # write the data to disk
+    # get filename from user or use default
     graph_filename = None
     if args:
         graph_filename = args.filename[0]
     else:
         graph_filename = 'star_wars_ontology.xml'
 
+    # write the data to disk
     graph.serialize(graph_filename, format='pretty-xml', encoding='utf-8')
-
-    # DEBUG - print the data for testing
-    # with open(graph_filename) as file:
-    #     for line in file:
-    #         print(line, end='')
 
 def main():
     parser = args_build_graph()
