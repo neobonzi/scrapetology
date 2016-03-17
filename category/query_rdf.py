b26d85fd3bd631e0c14b2768187c5587ddd2839d
jmcgover <jmcgover@calpoly.edu>
2016-03-17 00:20:19 -0700

    it's a piece of shit

--- category/query_rdf.py
+++ category/query_rdf.py
@@ -18,18 +18,16 @@ meh = '[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s'
 handler.setFormatter(formatter)
 LOGGER.addHandler(handler)
 
+QUERY='SELECT ?title WHERE { ?subject rdfs:subClassOf swdb:Category_Individual . ?subject owl:title ?title .}'
+
 def sparql_query(graph):
-    query = '''SELECT ?subject
-           WHERE {
-              ?subject rdfs:subClassOf* swdb:Category:Individual
-           }'''
+    query = QUERY
     LOGGER.debug('QUERY:QUERYING:%s', query)
     results = graph.query(query)
-    LOGGER.debug('QUERY:RESULTS :%s', query)
+    LOGGER.debug('QUERY:RESULTS :')
     for r in results:
         print(r)
     LOGGER.debug('QUERY:DONE')
-
 def main():
     LOGGER.setLevel(logging.INFO)
 
@@ -51,6 +49,7 @@ def main():
     LOGGER.info('Importing Graph: %s', graph_pickle_filename)
     with open(graph_pickle_filename, 'rb') as file:
         graph = pickle.load(file)
+    LOGGER.info('Done Importing')
     sparql_query(graph)
     return 0
 
